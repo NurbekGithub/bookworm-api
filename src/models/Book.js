6deb2dd449c87e1f2dac4d3bbc8d28316c64594b
Nurbek <nurbek-2395@mail.ru>
2017-12-28 00:14:12 +0600

    implemented add book

--- src/models/Book.js
+++ src/models/Book.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const schema = new mongoose.Schema({
   title: { type: String, required: true },
   authors: { type: String, required: true },
-  covers: { type: String, required: true },
+  cover: { type: String, required: true },
   goodreadsId: { type: String },
   pages: { type: String, required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, required: true }
